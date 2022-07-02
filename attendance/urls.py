c3773c975f396df063a7a927510b73fd18f63bca
[abhi2] <[abhi2907singh@gmail.com]>
2022-07-02 08:35:31 +0530

    form update

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.enrollmentForm, name='enrollmentForm'),
-    path('submitData', views.submitData, name='submitData'),
+    # path('submitData', views.submitData, name='submitData'),
     path('home',views.home,name='home'),
-    path('validateOTP',views.validateOTP,name="validateOTP"),
+    # path('validateOTP',views.validateOTP,name="validateOTP"),
     path('openWebCam',views.openWebCam,name='openWebCam'),
     path('isImageVarified',views.isImageVarified,name='isImageVarified')
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,10 +1,14 @@
+from ast import arg
 from asyncio.windows_events import NULL
+import email
 import math
 import random
+from cv2 import setTrackbarPos
+import django
 import face_recognition
 from django.core.mail import send_mail
 from django.http import HttpResponse, HttpResponseRedirect, StreamingHttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import cv2
 from django.views.decorators import gzip
 from requests import request
@@ -25,28 +29,48 @@ copy_instance = None
 
 otp_val = "0000"
 
+
+VID_FLAG = 0
+
 # Create your views here.
 
 def home(request):
-    list_data = []
-    val = EnrollStudent.objects.all()
-    print(val)
-    for i in val:
-        list_data.append(i)
-        print(i)
+    list_data = ['---Select---']
+    try:
+        val = EnrollStudent.objects.all()
+        print(val)
+        for i in val:
+            list_data.append(i)
+            print(i)
+    except Exception as e:
+        print(e)
+    if len(list_data) == 0:
+        list_data = "No Data in Database"
     return render(request, 'Enrollment.html',{'messages':list_data})
 
 @gzip.gzip_page
 def openWebCam(request):
-    try:
-        data = "abhishek"
-    except Exception as e:
-        print(e)
-    try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(request,cam,data), content_type="multipart/x-mixed-replace;boundary=frame")
-    except Exception as e:
-        print(e)
+    global VID_FLAG
+    if VID_FLAG == 0 or VID_FLAG == 1:
+        print("-----------------------------0------------------------------------------")
+        try:
+            data = request.GET['person']
+        except Exception as e:
+            print(e)
+        try:
+            cam = VideoCamera()
+            genval = gen(request,cam,data)
+            print(f"%%%%%%%%%%%%%%%%%%%    {type(genval)}")
+            if VID_FLAG == 1:
+                print("working bhai")
+                VID_FLAG = 0
+                return redirect("home")
+            return StreamingHttpResponse(genval, content_type="multipart/x-mixed-replace;boundary=frame")
+        except Exception as e:
+            print(e)
+    elif VID_FLAG == 1:
+        print("------------------------------------1---------------------------------------")
+        return render(request,'Enrollment.html',{'value':True})
     return render(request,'Enrollment.html')
 
 def isImageVarified(request):
@@ -54,64 +78,63 @@ def isImageVarified(request):
 
 
 def enrollmentForm(request):
-    form = EnrollmentForm()
-    return render(request, 'Signup.html', {'form': form})
-
-
-def submitData(request):
-    global student_object
-    global copy_instance
+    name = ""
+    emailId= ""
+    sid = ""
+    img = ""
     if request.method == 'POST':
-        print('fetching form data')
+        
+        if 'signupbtn' in request.POST:
+            print('fetching form data')
         # student = EnrollmentForm(request.POST, request.FILES)
-        student_object.name = request.POST['name']
-        student_object.emailId = request.POST['email']
-        student_object.sid = request.POST['sid']
-        student_object.img = request.FILES['imgup']
+            name = request.POST['name']
+            emailId = request.POST['email']
+            sid = request.POST['sid']
+            if validate_data(name,sid) and send_otp(emailId):
+                return render(request,'Signup.html',{'flag':0,'name':name,'sid':sid,'emailId':emailId})
+        elif 'otpbtn' in request.POST:
+            firstVal = request.POST['input1']
+            secondVal = request.POST['input2']
+            thirdVal = request.POST['input3']
+            fourthVal = request.POST['input4']
+            name = request.POST['name']
+            emailId = request.POST['email']
+            sid = request.POST['sid']
+            img = request.FILES['imgup']
 
-        copy_instance = student_object
-        print(student_object.img)
-        if validate_data(student_object) and send_otp(student_object.emailId):
-            return render(request,'checkOtp.html')
-        return HttpResponseRedirect('/')
-    
-    
-def validateOTP(request):
-    global student_object
-    if request.method == 'GET':
-        print("OTP verifing")
-        firstVal = request.GET['input1']
-        secondVal = request.GET['input2']
-        thirdVal = request.GET['input3']
-        fourthVal = request.GET['input4']
+            val = firstVal+secondVal+thirdVal+fourthVal
 
-        val = firstVal+secondVal+thirdVal+fourthVal
-
-        print(f'OTP genrated is: {otp_val} and OTP user entered is: {val}')
-        print(f'OTP genrated is: {type(otp_val)} and OTP user entered is: {type(val)}')
-        if(str(val)==str(otp_val)):
-            print("OTP varified")
-            if copy_instance!=None:
-                copy_instance.save()
+            print(f'OTP genrated is: {otp_val} and OTP user entered is: {val}')
+            print(f'OTP genrated is: {type(otp_val)} and OTP user entered is: {type(val)}')
+            if(str(val)==str(otp_val)):
+                print("OTP varified")
+                print(f'{name} {sid} {emailId}')
+                student_object = EnrollStudent()
+                student_object.name = name
+                student_object.sid = sid
+                student_object.emailId = emailId
+                student_object.img = img
+                print(img)
+                if student_object!=None:
+                    print(student_object)
+                    student_object.save()
+                else:
+                    print("Something went erong")
+                    return HttpResponse("Wrong")
+                student_object.clean()
+                print("OTP varified sucessfully with image")
+                return render(request,'SignUp.html',{'flag':1})
             else:
-                print("Something went erong")
-                return HttpResponse("Wrong")
-            # if not detect_face(student_object.img):
-            #     messages.add_message(request,1,"Face not recognised")
-            #     student_object.delete()
-            #     print("Face not found")
-            #     return render(request,'Signup.html')
-            student_object.clean()
-            print("OTP varified sucessfully with image")
-            return render(request,'SignUp.html')
-        else:
-            return HttpResponseRedirect('/')
+                print("otp not varified")
+                return HttpResponseRedirect('/')
 
-    return HttpResponse("<h1>OTP Not Matched</h1>")
+        return HttpResponse("<h1>OTP Not Matched</h1>")
+    return render(request, 'Signup.html', {'flag':1})
 
 
 class VideoCamera(object):
     def __init__(self):
+        self.waiting_time = 0
         self.video = cv2.VideoCapture(0)
         (self.grabbed, self.frame) = self.video.read()
         threading.Thread(target=self.update, args=()).start()
@@ -121,31 +144,55 @@ class VideoCamera(object):
 
     def get_frame(self,request,imageName):
         jpeg = self.frame
-        aceloc = face_recognition.face_locations(jpeg)
-        print(aceloc)
         try:
+           
+            aceloc = face_recognition.face_locations(jpeg)
+            print(aceloc)
             if len(aceloc)==1:
                 aceloc = aceloc[0]
-                
                 faceencode = face_recognition.face_encodings(jpeg)
-                faceencode = faceencode[0]
+      
+                if len(faceencode) == 1:
+                    faceencode = faceencode[0]
+            
                 cv2.rectangle(jpeg, (aceloc[3], aceloc[0]), (aceloc[1], aceloc[2]), (255, 0, 255), 2)
+       
                 auth = EnrollStudent.objects.get(name=imageName)
-                imgvar = face_recognition.load_image_file(f"media/{auth.img}")
-                imgvar = cv2.cvtColor(imgvar,cv2.COLOR_BGR2RGB)
-                imgVar = cv2.rotate(imgVar, cv2.ROTATE_90_COUNTERCLOCKWISE)
+             
+                try:
+                    imgvar = face_recognition.load_image_file(f"media/{auth.img}")
+                    print(f"media/{auth.img}")
+                    # imgvar = cv2.cvtColor(imgvar,cv2.COLOR_BGR2RGB)
+                    # imgVar = cv2.rotate(imgVar, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                except Exception as e:
+                    print("there is some issue in line number 162 ",e)
+   
                 faceloc = face_recognition.face_locations(imgvar)
+              
                 if len(faceloc) == 1:
                     faceencode2 = face_recognition.face_encodings(imgvar)[0]
-                    flag = face_recognition.compare_faces(faceencode2,faceencode)
-                    if flag==True:
-                        return isImageVarified(request)
-                    else:
-                        return HttpResponse("Not")
+                    # try:
+                    #     # print(faceencode2)
+                    #     # print(faceencode)
+                    #     flag = face_recognition.compare_faces(faceencode2,faceencode)
+                    # except Exception as e:
+                    #     print("Image compare Execption")
+                    # if flag==True:
+                    #     print("True image face detected")
+                    #     return isImageVarified(request)
+                    # else:
+                    #     print("not detected")
+                    #     return HttpResponse("Not")
         except Exception as e:
             self.video.release()
-            print(e)
-
+            print("there is some issue in line number 175 ",e)
+        self.waiting_time += 1
+        print(self.waiting_time)
+        global VID_FLAG
+        if self.waiting_time == 10:
+            self.video.release()
+            VID_FLAG = 1
+            return HttpResponse("hello")
         _, jpeg = cv2.imencode('.jpg', jpeg)
         return jpeg.tobytes()
 
@@ -157,17 +204,21 @@ class VideoCamera(object):
 def gen(request,camera,data):
     while True:
         frame = camera.get_frame(request,data)
+        if isinstance(frame,HttpResponse):
+            print("hello bhai")
+            setTrackbarPos(10)
+            return redirect('Enrollment.html')
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-def validate_data(param)->bool:
-    if len(param.name) < 2:
+def validate_data(name,sid)->bool:
+    if len(name) < 2:
         return False
-    temp = param.sid
 
-    if len(temp) < 7 or not re.match(validstudentId,temp):
+    if len(sid) < 7 or not re.match(validstudentId,sid):
         return False
+
 
     return True
 
